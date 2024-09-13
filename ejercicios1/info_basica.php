9a3fc9231a946df3ca76d51ce5182eca83952425
David <davnundia@alu.edu.gva.es>
2024-09-13 13:20:25 +0200

    commit1 ej1

---
+++ ejercicios1/info_basica.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>info_basica</title>
+</head>
+<body>
+    <?php
+        $name = "David";
+        $year_birth =  "2004"
+    ?>
+    <h1>My name is <?php echo $name ?> and i was born in <?php echo $year_birth ?></h1>
+</body>
+</html>
