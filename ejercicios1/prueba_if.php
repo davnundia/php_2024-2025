af254f2d6ef6dda1c85dd19be945577f8f419b6b
David <davnundia@alu.edu.gva.es>
2024-09-14 12:48:02 +0200

    commit2 ej4

--- ejercicios1/prueba_if.php
+++ ejercicios1/prueba_if.php
@@ -12,26 +12,42 @@
 </style>
 <body>
     <?php
-        $mark1 = $_REQUEST["mark1"];
-        $mark2 = $_REQUEST["mark2"];
-        if($mark1 > $mark2){
-            $mayor = $mark1;
-        }
-        else{
-            $mayor = $mark2;
+        $visibility = false;
+
+        if(isset($_REQUEST["Send"])){
+            $mark1 = $_REQUEST["mark1"];
+            $mark2 = $_REQUEST["mark2"];
+            $mark3 = $_REQUEST["mark3"];
+
+            if($mark1 > $mark2 AND $mark1 > $mark3){
+                $highest = $mark1;
+                $highest_mark = "Mark1";
+            }
+            else if ($mark2 > $mark1 AND $mark2 > $mark3){
+                $highest = $mark2;
+                $highest_mark = "Mark2";
+            }
+            else{
+                $highest = $mark3;
+                $highest_mark = "Mark3";
+            }
+
+            $visibility = true;
         }
     ?>
 
-    <h1>The highest number</h1>
+    <h1>The highest mark</h1>
 
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <label for="mark1">Number1:</label><br>
+        <label for="mark1">Mark1:</label><br>
         <input type="number" name="mark1"><br><br>
-        <label for="mark2">Number2:</label><br>
+        <label for="mark2">Mark2:</label><br>
         <input type="number" name="mark2"><br><br>
-        <input type="submit" value="Enviar">
+        <label for="mark2">Mark3:</label><br>
+        <input type="number" name="mark3"><br><br>
+        <input type="submit" name="Send" value="Send">
     </form>
 
-    <h2>The highest number is <?php echo $mayor ?> </h2>
+    <h2 style="visibility: <?php echo $visibility ? 'visible' : 'hidden'; ?>;">The highest mark is <?php echo $highest_mark ?> with a <?php  echo $highest ?> </h2>
 </body>
 </html>
