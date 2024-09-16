64b77f052dbf8a14977695743b431c1d6dcafccc
David <davnundia@alu.edu.gva.es>
2024-09-16 11:41:06 +0200

    commit2 ej6

--- ejercicios1/array/array1.php
+++ ejercicios1/array/array1.php
@@ -5,7 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>array1</title>
 </head>
+<style>
+    h1{
+        text-align:center;
+    }
+    div{
+        padding: 15px;
+    }
+</style>
 <body>
+    <h1>Array with 50 random numbers and others filters</h1>
     <?php
         //create random numbers between 0-99
         function rand_in_range($start, $end) {
@@ -15,7 +24,8 @@
         $array = [];
 
         //create the array with 50 numbers
-        function fill_array($array){
+        function fill_array(){
+            global $array;
             for ($count=0; $count <= 50; $count++) { 
                 $num = rand_in_range(0, 99);
                 //if the number is in the array turn back and repeat the loop
@@ -30,7 +40,8 @@
         
 
         //print the array as a list with all the values
-        function print_list($array){
+        function print_list(){
+            global $array;
             echo "<ul>";
             foreach ($array as $value) {
                 echo "<li>$value</li>";
@@ -40,9 +51,9 @@
 
 
         //order the list
-        function order_list($array, $length){
+        function order_list($length){
+            global $array;
             $end_loop = true;
-            
             for ($position=0; $position < $length - 1; $position++) { 
                 $first = $array[$position];
                 $second = $array[$position + 1];
@@ -55,18 +66,43 @@
             }
             $length--;
             if(!$end_loop and $length > 1){
-                order_list($array, $length);
+                order_list($length);
             }
         }
 
 
         //execution area
-        fill_array($array);
+        fill_array();
         $length = count($array);
-        order_list($array, $length);
-        print_list($array);
+        order_list($length);
+        print_list();
 
     ?>
-
+    <div>
+        <h2>The highest number</h2>
+        <?php
+            //Find the highest number
+            echo max($array)
+        ?>
+    </div>
+    <div>
+        <h2>The lowest number</h2>
+        <?php
+            //Find the lowest number
+            echo min($array)
+        ?>
+    </div>
+    <div>
+        <h2>Average</h2>
+        <?php
+            //Find the average
+            $suma = 0;
+            foreach ($array as $value) {
+                $suma = $suma + $value;
+            }
+            $average = $suma/$length;
+            echo ($average);
+        ?>
+    </div>
 </body>
 </html>
