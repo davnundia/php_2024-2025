b148a6a5d3ddd24045236dcf28c5a052cb69c733
David <davnundia@alu.edu.gva.es>
2024-09-17 12:43:20 +0200

    commit2 ej12

--- ejercicios1/funciones/intercambia.php
+++ ejercicios1/funciones/intercambia.php
@@ -5,13 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>intercambia</title>
 </head>
+<style>
+    p{
+        font-size:20px;
+    }
+</style>
 <body>
+    <h1>change the numbers for reference</h1>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <label for="first_num">Number A :</label><br>
         <input type="number" name="first_num" id=""><br><br>
         <label for="second_num">Number B :</label><br>
-        <input type="number" name="second_num" id=""><br>
-        <input type="submit" name="Send">
+        <input type="number" name="second_num" id=""><br><br>
+        <input type="submit" name="Send"><br><br>
     </form>
     <?php
         $a = $_REQUEST["first_num"];
@@ -26,8 +32,8 @@
 
         if (isset($_REQUEST["first_num"]) && isset($_REQUEST["second_num"])) {
             change($a, $b);
-            echo "The number A, now is: " . $a . "<br>";
-            echo "The number A, now is: " . $b;
+            echo "<p>The number A, now is: " . $a . "</p><br>";
+            echo "<p>The number A, now is: " . $b . "</p>";
         }
     ?>
 </body>
