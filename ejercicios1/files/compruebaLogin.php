3f8f7c1bade28dcd95976ae5cb04132eb3aed3c3
davnundia <davnundia@alu.edu.gva.es>
2024-10-10 19:50:51 +0200

    commit9

---
+++ ejercicios1/files/compruebaLogin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comprobar Login</title>
+</head>
+<body>
+    <?php
+        include "signin.php";
+
+        
+    ?>
+    
+</body>
+</html>
